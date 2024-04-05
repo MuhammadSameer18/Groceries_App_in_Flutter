ec4f79d5fae76be7f972a453f614c1d18ad63725
MuhammadSameer18 <muhammadsameermukhtar@gmail.com>
2024-04-05 21:57:18 +0500

    today country picker setup

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -1,7 +1,17 @@
+import 'package:fl_country_code_picker/fl_country_code_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-class SignInScreen extends StatelessWidget {
+class SignInScreen extends StatefulWidget {
   const SignInScreen({super.key});
+
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  final countryPicker = const FlCountryCodePicker();
+  CountryCode? countryCode;
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +30,51 @@ class SignInScreen extends StatelessWidget {
           ),
           Column(
             children: [
-              TextField(),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                textInputAction: TextInputAction.done,
+                maxLines: 1,
+                decoration: InputDecoration(
+                  labelText: "Enter Your Number",
+                  prefixIcon: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 6),
+                    margin: EdgeInsets.symmetric(horizontal: 8),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        GestureDetector(
+                          onTap: () async {
+                            final code = await countryPicker.showPicker(
+                                context: context);
+                            setState(() {
+                              countryCode = code;
+                            });
+                          },
+                          child: Row(
+                            children: [
+                              Container(
+                                child: countryCode != null
+                                    ? countryCode!.flagImage()
+                                    : null,
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Container(
+                                child: Text(
+                                  countryCode?.dialCode ?? "+1",
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  labelStyle: TextStyle(color: Colors.grey),
+                ),
+              ),
             ],
           ),
         ],
